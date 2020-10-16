75e6dfa55fc334f802d721ec6fbde236ec353fec
Daniel Luder <daniel.luder@students.bfh.ch>
2020-10-16 17:13:47 +0200

    restructured settings.xlsx by introducing variables to remove NaNs

--- PEC2struct_Test.m
+++ PEC2struct_Test.m
@@ -5,14 +5,14 @@ end
 
 %% Test automatic PEC export for backups
 function test_case_1_automatic_export_one_cell(testCase)
-actSolution = PEC2struct('PEC2struct_TEST\test_case_1_automatic_export_one_cell.csv');
+actSolution = PEC2struct('PEC2struct_Test\test_case_1_automatic_export_one_cell.csv');
 load('PEC2struct_TEST\test_case_1_automatic_export_one_cell.mat', 'expSolution');
 verifyEqual(testCase,actSolution,expSolution)
 end
 
 %% Test manual PEC export for tests with three cells
 function test_case_2_full_manual_export_three_cells(testCase)
-actSolution = PEC2struct('PEC2struct_TEST\test_case_2_full_manual_export_three_cells.csv');
+actSolution = PEC2struct('PEC2struct_Test\test_case_2_full_manual_export_three_cells.csv');
 load('PEC2struct_TEST\test_case_2_full_manual_export_three_cells.mat', 'expSolution');
 verifyEqual(testCase,actSolution,expSolution)
 end
